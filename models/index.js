090e2d5c0ba19c480722678c6ede75e803521938
Adrian <ablazleon@ono.com>
2018-06-11 19:16:41 +0200

    Step 10 - Heroku Postgres.

--- models/index.js
+++ models/index.js
@@ -5,7 +5,11 @@ const path = require('path');
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("sqlite:quiz.sqlite");
+// const sequelize = new Sequelize("sqlite:quiz.sqlite");
+
+const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+
+const sequelize = new Sequelize(url);
 
 // Import the definition of Quiz Table from quiz.js
 sequelize.import(path.join(__dirname, 'quiz'));
